de4e8c8bf3506f4e93e5462c1742d256ac05d90b
wanhello <wankeye@foxmail.com>
2019-09-13 11:49:54 +0800

    ns

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -9,39 +9,91 @@ import (
 	"github.com/urfave/cli"
 )
 
-func NewCommmand() cli.Command {
+// NewCommand 创建项目命令
+func NewCommand() cli.Command {
 	return cli.Command{
-		Name:                   "new",
-		Aliases:                []string{"n"},
-		Usage:                  "",
-		UsageText:              "",
-		Description:            "",
-		ArgsUsage:              "",
-		Category:               "",
-		BashComplete:           nil,
-		Before:                 nil,
-		After:                  nil,
-		Action:                 func(ctx *cli.Context) error {
+		Name:    "new",
+		Aliases: []string{"n"},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "dir, d",
+				Usage: "项目生成目录",
+			},
+			&cli.StringFlag{
+				Name:  "pkg, p",
+				Usage: "项目包名",
+			},
+			&cli.BoolFlag{
+				Name:  "mirror, m",
+				Usage: "使用国内镜像(gitee.com)",
+			},
+		},
+		Action: func(c *cli.Context) error {
 			cfg := new.Config{
-				Dir: c.String("dir"),
+				Dir:       c.String("dir"),
+				PkgName:   c.String("pkg"),
+				UseMirror: c.Bool("mirror"),
 			}
+			return new.Exec(cfg)
 		},
-		OnUsageError:           nil,
-		Subcommands:            nil,
-		Flags:                  []cli.Flag{
-
-		},
-		SkipFlagParsing:        false,
-		SkipArgReorder:         false,
-		HideHelp:               false,
-		Hidden:                 false,
-		UseShortOptionHandling: false,
-		HelpName:               "",
-		CustomHelpTemplate:     "",
 	}
-
 }
 
+// GenerateCommand 生成项目模块命令
+func GenerateCommand() cli.Command {
+	return cli.Command{
+		Name:    "generate",
+		Aliases: []string{"g"},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "dir, d",
+				Usage:    "项目生成目录",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "pkg, p",
+				Usage:    "项目包名",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:  "name, n",
+				Usage: "模块名称(结构体名称)",
+			},
+			&cli.StringFlag{
+				Name:  "comment, c",
+				Usage: "模块注释",
+			},
+			&cli.StringFlag{
+				Name:  "file, f",
+				Usage: "指定模板文件(.json，模板配置可参考说明)",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			cfg := generate.Config{
+				Dir:     c.String("dir"),
+				PkgName: c.String("pkg"),
+				Name:    c.String("name"),
+				Comment: c.String("comment"),
+				File:    c.String("file"),
+			}
 
+			if cfg.Dir == "" {
+				fmt.Println("请指定项目目录")
+				return nil
+			} else if cfg.PkgName == "" {
+				fmt.Println("请指定包名")
+				return nil
+			} else if cfg.Name == "" && cfg.File == "" {
+				fmt.Println("请指定模块名称或模板文件")
+				return nil
+			} else if cfg.Name != "" && cfg.Comment == "" {
+				fmt.Println("请指定模块说明")
+				return nil
+			}
+
+			return generate.Exec(cfg)
+		},
+	}
+}
 
 
--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+	"os"
+	"github.com/wanhello/iris-admin-cli/cmd"
+	"github.com/urfave/cli"
+)
+
 func main()  {
 	
+	app := cli.NewApp()
+	app.Name = "gin-admin-cli"
+	app.Description = "GinAdmin辅助工具"
+	app.Version = "0.2.0"
+	app.Commands = []cli.Command{
+		cmd.NewCommand(),
+		cmd.GenerateCommand(),
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
 }
